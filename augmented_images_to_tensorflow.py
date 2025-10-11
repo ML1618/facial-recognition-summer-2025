402b098f643259a387dd6d275bf50c7af527d4fe
Milo Lee <143678373+ChronosWithA-K@users.noreply.github.com>
2025-10-11 13:34:46 -0700

    fix the rest of the variables

--- augmented_images_to_tensorflow.py
+++ augmented_images_to_tensorflow.py
@@ -11,13 +11,13 @@ train_images = train_images.map(lambda x: tf.image.resize(x, (120, 120))) # Make
 train_images = train_images.map(lambda x: x / 255.0) # Lets us apply sigmoid activation to final layer of neural network
 
 test_images = tf.data.Dataset.list_files('aug_data\\test\\images\\*.jpg', shuffle=False)
-test_images = train_images.map(load_image)
-test_images = train_images.map(lambda x: tf.image.resize(x, (120, 120)))
-test_images = train_images.map(lambda x: x / 255.0)
+test_images = test_images.map(load_image)
+test_images = test_images.map(lambda x: tf.image.resize(x, (120, 120)))
+test_images = test_images.map(lambda x: x / 255.0)
 
 val_images = tf.data.Dataset.list_files('aug_data\\val\\images\\*.jpg', shuffle=False)
-val_images = train_images.map(load_image)
-val_images = train_images.map(lambda x: tf.image.resize(x, (120, 120)))
-val_images = train_images.map(lambda x: x / 255.0)
+val_images = val_images.map(load_image)
+val_images = val_images.map(lambda x: tf.image.resize(x, (120, 120)))
+val_images = val_images.map(lambda x: x / 255.0)
 
 print(train_images.as_numpy_iterator().next())
